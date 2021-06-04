fea5dd0d6c061853713f41c35a8de6bf8f1094b1
Samat <Asylbekuulusamat555@gmail.com>
2021-06-04 14:48:36 +0600

    3-Our Review

---
+++ src/data.js
@@ -0,0 +1,50 @@
+ const reviews = [
+   {
+     id: 1,
+     name: "John Doe",
+     job: "web developer",
+     image:
+       "https://images.generated.photos/7fQIk3HHDrptS2-OPXmde5DdjhBFNBXUBkWsV4a1dXE/rs:fit:256:256/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA2MDEzNjQuanBn.jpg",
+     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate et vitae doloremque voluptatibus officia quasi deserunt beatae consequatur cumque! Hic laborum odit nihil aut placeat delectus quisquam ratione labore repellat natus. Laboriosam necessitatibus, ad dolores quae nemo amet quibusdam doloribus!",
+   },
+   {
+     id: 2,
+     name: "Michael Richard",
+     job: "Frontend",
+     image: "https://i.imgur.com/GLwgYL4.jpg",
+     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate et vitae doloremque voluptatibus officia quasi deserunt beatae consequatur cumque! Hic laborum odit nihil aut placeat delectus quisquam ratione labore repellat natus. Laboriosam necessitatibus, ad dolores quae nemo amet quibusdam doloribus!",
+   },
+   {
+     id: 3,
+     name: "Joseph Thomas",
+     job: "Backend",
+     image:
+       "https://images.generated.photos/_rfbm9RisttH-tTHQ0Kkqxx1u-I-4KyeChs55lovRJs/rs:fit:256:256/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzAwMDc3MzYuanBn.jpg",
+     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate et vitae doloremque voluptatibus officia quasi deserunt beatae consequatur cumque! Hic laborum odit nihil aut placeat delectus quisquam ratione labore repellat natus. Laboriosam necessitatibus, ad dolores quae nemo amet quibusdam doloribus!",
+   },
+   {
+     id: 4,
+     name: "Daniel Edward",
+     job: "Full-Stack",
+     image:
+       "http://s1.iconbird.com/ico/0612/customicondesignoffice2/w256h2561339870331Man256.png",
+     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate et vitae doloremque voluptatibus officia quasi deserunt beatae consequatur cumque! Hic laborum odit nihil aut placeat delectus quisquam ratione labore repellat natus. Laboriosam necessitatibus, ad dolores quae nemo amet quibusdam doloribus!",
+   },
+   {
+     id: 5,
+     name: "John Foster",
+     job: "SEO",
+     image:
+       "https://secure.gravatar.com/avatar/d6a4ac460a17779cc721f150922cc927?s=256&d=mm&r=g",
+     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate et vitae doloremque voluptatibus officia quasi deserunt beatae consequatur cumque! Hic laborum odit nihil aut placeat delectus quisquam ratione labore repellat natus. Laboriosam necessitatibus, ad dolores quae nemo amet quibusdam doloribus!",
+   },
+   {
+     id: 6,
+     name: "Anthony Kevin",
+     job: "Manager",
+     image:
+       "http://www.regiscorp.com/content/dam/supercuts/www/models/2019-models/4x3/supercuts-model-nate-1-skin-fade-high-fade-4x3.png",
+     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate et vitae doloremque voluptatibus officia quasi deserunt beatae consequatur cumque! Hic laborum odit nihil aut placeat delectus quisquam ratione labore repellat natus. Laboriosam necessitatibus, ad dolores quae nemo amet quibusdam doloribus!",
+   },
+ ];
+export default reviews;
